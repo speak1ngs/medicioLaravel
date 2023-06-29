e581db3a1a4bfdaaafc352e7aac4fcd9c25a6911
speak1ngs <juanmavillalbaovelar@gmail.com>
2023-06-29 16:38:36 -0400

    se agrego un modal, se ajustaron los modales de mensajes, se creo la funcionalidad de estados de la citas

--- app/Http/Livewire/DoctorAgenda.php
+++ app/Http/Livewire/DoctorAgenda.php
@@ -9,36 +9,93 @@ use Livewire\WithPagination;
 class DoctorAgenda extends Component
 {
     public $inputRating , $inputComent;
-    public $nom, $ide;
+    public $nom, $ide, $control, $val, $buttonState ,$statPaciente, $idpacen;
+    public $cant = 10;
+    public $stat = 1;
     use WithPagination;
 
     public function mount() 
     {
-       
+        $this->control = "successComent";
+        $this->statPaciente= "presente";
+        $this->cant= 10;
+        $this->stat = 1 ;
     }
 
-    public function instanData($iden, $nomb) 
+    public function instanData($iden, $nomb, $idpac) 
+    {   
+        $this->reset(['ide','nom','idpacen']);
+        $this->ide = $iden;
+        $this->nom = $nomb;
+        $this->idpacen = $idpac;
+        $this->val = db::table('citas')->where('id','=',$this->ide)->get('calificacion_status_id');
+        if($this->val[0]->calificacion_status_id !=2 ){
+            $this->control = "failComment";
+        }
+
+    }
+
+    public function ausente($iden, $nomb) 
     {   
         $this->reset(['ide','nom']);
         $this->ide = $iden;
         $this->nom = $nomb;
+        $this->stat = 1 ;
+        $this->statPaciente= "ausente";
     }
+
+    public function presente($iden, $nomb) 
+    {
+        $this->reset(['ide','nom']);
+        $this->ide = $iden;
+        $this->nom = $nomb;
+        $this->stat = 2 ;
+        $this->statPaciente= "presente";
+    }
+
+    public function setStatDate() 
+    {
+        try {
+            db::table('citas')->where('id','=',$this->ide)->update([
+                'paciente_status_id' => $this->stat
+            ]);
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+    }
+
 
     public function calificar()
     {   
-        $val = db::table('citas')->where('id','=',$this->ide)->get('calificacion_status_id');
-        if($val['calificacion_status_id'] ===2 ){
-        db::table('citas')->where('id','=',$this->ide)->update([
-            'descripcion_doctor' => $this->inputComent,
-            'cal_doc_id' => $this->inputRating,
-            'calificacion_status_id' => 1
-        ]);
-    }
+        
+        try {
+            if($this->control === "successComent" ){
+            
+                db::table('citas')->where('id','=',$this->ide)->update([
+                'descripcion_doctor' => $this->inputComent,
+                'cal_doc_id' => $this->inputRating,
+                'calificacion_status_id' => 1
+            ]);
+                $this->control = "successComent";
+                session()->flash('message', 'Se califico al paciente.');    
+            }
+            else{
+            $this->reset(['inputRating','inputComent']);
+                session()->flash('message', 'Ya se califico al paciente.');    
+            }
+
+        } catch (\Throwable $th) {
+            //throw $th;
+            $this->control = "failComment";
+            session()->flash('message', 'Fallo la conexion a la db.');
+        }
+    
  
     }
 
     public function render()
     {
+        // agregar id del doctor a nivel global
         $db= DB::table('citas')
         ->join('calendarios_detalles', 'citas.calendarios_deta_id','=','calendarios_detalles.id')
         ->join('calendarios_doctores','calendarios_detalles.calendarios_doctores_id','=','calendarios_doctores.id')
@@ -48,12 +105,13 @@ class DoctorAgenda extends Component
         ->join('personas','pacientes.persona_id','=', 'personas.id')
         ->join('citas_status','citas.status_id','=','citas_status.id')
         ->select('citas.id',DB::raw('CONCAT(personas.nombre," ", personas.apellido) as nombres'),'calendarios_detalles.dias_laborales', 'calendarios_detalles.horarios', 'citas_status.descripcion', 
-                'consultorios.nombre as consul_nomb'
+                'consultorios.nombre as consul_nomb', 'doctores.id as iddoc', 'citas.paciente_id as idpac'
                 )
         ->where('citas_status.id','=',1)
         ->where('calendarios_detalles.dias_laborales', '<', '2023-08-08')
         ->where('citas.paciente_status_id','=',3)
-        ->paginate(10);
+        ->where('doctores.id','=', 1)
+        ->paginate($this->cant);
 
         return view('livewire.doctor-agenda', compact('db'));
     }
--- resources/views/livewire/doctor-agenda.blade.php
+++ resources/views/livewire/doctor-agenda.blade.php
@@ -7,9 +7,18 @@
 	<div class="container well">
 
 				<div class="col-md-12">
-					<h4>Citas programadas </h4>
+					<h4 class="text-center">Citas programadas </h4>
 					<div class="table-responsive">
-
+							<div class="form-group col-md-1 ">
+								<label for="">Mostrar</label>
+								<select id="inputState" class="form-control" wire:model="cant">
+										
+										<option value="10">10</option>
+										<option value="25">25</option>
+										<option value="50">50</option>
+										<option value="100">100</option>
+								</select>
+							</div>
 
 						<table id="mytable" class="table table-bordred table-striped">
 
@@ -34,7 +43,7 @@
 									<td>
 										
 										<span data-title="calf" data-toggle="modal" data-target="#calf"
-									data-dismiss="modal" wire:click.prevent="instanData('{{ $data->id }}','{{ $data->nombres }}')">Calificar</span>
+									data-dismiss="modal" wire:click.prevent="instanData('{{ $data->id }}','{{ $data->nombres }}', '{{ $data->idpac }}')" wire:ignore.self>Calificar</span>
 							
 
 									</td>
@@ -42,13 +51,17 @@
 										<div class="row">
 											<div class="col-sm-4 bottom-p">
 												<p data-placement="top" data-toggle="tooltip" title="ausente" class="bottom-p">
-													<button class="btn btn-danger btn-xs"><span
+													<button class="btn btn-danger btn-xs" data-title="confirmModal" data-toggle="modal" data-target="#confirmModal"
+							data-dismiss="modal" wire:click.prevent="ausente('{{ $data->id }}','{{ $data->nombres }}')"><span
 															class="fa fa-remove"></span></button>
 												</p>
 											</div>
 											<div class="col-sm-4 bottom-p">
 												<p data-placement="top" data-toggle="tooltip" title="presente" class="bottom-p">
-													<button class="btn btn-success btn-xs"><span
+													<button class="btn btn-success btn-xs"
+													data-title="confirmModal" data-toggle="modal" data-target="#confirmModal"
+							data-dismiss="modal" wire:click.prevent="presente('{{ $data->id }}','{{ $data->nombres }}')"
+													><span
 															class="fa fa-check-square-o"></span></button>
 												</p>
 
@@ -67,7 +80,11 @@
 							</tbody>
 						
 						</table>
-
+						@if($db->hasPages())
+						<div class="divpag">
+							{{ $db->link()}}
+						</div>
+						@endif
 						
 
 					</div>
@@ -101,16 +118,17 @@
 										@endforeach
 									
 									</div>
+									
 								</div>
-							
+
 							</div>
 				
 							
 					</div>
 					<div class="modal-footer ">
-						<button type="button" class="btn btn-warning btn-lg" style="width: 100%;"
-							data-title="successComent" data-toggle="modal" data-target="#successComent"
-							data-dismiss="modal" wire:click="calificar()" ><span class="glyphicon glyphicon-ok-sign" ></span>Calificar</button>
+						<button type="button" class="btn btn-warning btn-lg" style="width: 100%;" wire:click="calificar()"
+							data-title="{{ $control }}" data-toggle="modal" data-target="#{{ $control }}"
+							data-dismiss="modal"  ><span class="glyphicon glyphicon-ok-sign" ></span>Calificar</button>
 					</div>
 				</div>
 				<!-- /.modal-content -->
@@ -124,7 +142,7 @@
 
 	<div class="container">
 		<div class="modal fade" id="successComent" tabindex="-1" role="dialog" aria-labelledby="successComent"
-			aria-hidden="true">
+			aria-hidden="true"  wire:ignore.self>
 			<div class="modal-dialog modal-confirm" role="document">
 				<div class="modal-content ">
 					<div class="modal-header">
@@ -134,8 +152,17 @@
 						<h4 class="modal-title w-100">Profesional Calificado!</h4>
 					</div>
 					<div class="modal-body">
-						<p class="text-center">Gracias por tu comentario!!</p>
+						<p class="text-center">Gracias por tu comentario {{ $control }}!!</p>
 					</div>
+					@if (session()->has('message'))
+
+					<div class="alert alert-success">
+
+						{{ session('message') }}
+
+					</div>
+
+					@endif
 					<div class="modal-footer">
 						<button class="btn btn-success btn-block" data-dismiss="modal">OK</button>
 					</div>
@@ -150,7 +177,7 @@
 
 	<div class="container">
 		<div class="modal fade" id="failComment" tabindex="-1" role="dialog" aria-labelledby="failComment"
-			aria-hidden="true">
+			aria-hidden="true" wire:ignore.self>
 			<div class="modal-dialog modal-confirm-red" role="document">
 				<div class="modal-content ">
 					<div class="modal-header">
@@ -163,6 +190,15 @@
 					</div>
 					<div class="modal-body">
 						<p class="text-center">Intente nuevamente</p>
+						@if (session()->has('message'))
+
+							<div class="alert alert-success">
+
+								{{ session('message') }}
+
+							</div>
+
+							@endif
 					</div>
 					<div class="modal-footer">
 						<button class="btn btn-success btn-block" data-dismiss="modal">OK</button>
@@ -173,6 +209,35 @@
 			<!-- /.modal-dialog -->
 		</div>
 	</div>
+
+
+	<div class="container">
+		<div class="modal fade" id="confirmModal" tabindex="-1" role="dialog" aria-labelledby="confirmModal"
+			aria-hidden="true" >
+			<div class="modal-dialog " role="document">
+				<div class="modal-content ">
+				
+									
+						<div class="alert alert-warning modal-title">
+						<button type="button" class="close " data-dismiss="modal" aria-hidden="true"><span
+								class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>
+								<br>
+							<span>El paciente estuvo {{ $statPaciente }}?</span>
+							
+							<div class="mt-5">
+								<button class="btn btn-danger btn-lg" data-dismiss="modal">Cancelar</button>	
+								<button class="btn btn-success btn-lg" data-dismiss="modal" wire:click="setStatDate">Confirmar</button>
+										
+							</div>
+					</div>
+				
+				</div>
+				<!-- /.modal-content -->
+			</div>
+			<!-- /.modal-dialog -->
+		</div>
+	</div>
+
 
 	<script>
     
